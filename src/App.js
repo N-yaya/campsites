9aa08aeab8ea0e81dd2c911c8e2f317220dc7dad
nassour <nassouryaya1993@gmail.com>
2024-04-29 12:35:53 +0200

    about page update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import CampsiteDetailPage from './pages/CampsiteDetailPage';
 import {Routes, Route} from 'react-router-dom';
 import ContactPage from './pages/ContactPage';
 import HomePage from './pages/HomePage';
-import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage'
+import CampsitesDirectoryPage from './pages/CampsitesDirectoryPage';
+import AboutPage from './pages/AboutPage';
 import Footer from './components/Footer';
 import './App.css';
 
@@ -17,6 +18,7 @@ function App() {
             <Route path='/' element={<HomePage/>}/>
             <Route path='Contact' element={<ContactPage/>}/>
             <Route path='directory' element={<CampsitesDirectoryPage/>}/>
+            <Route path='about' element={<AboutPage/>}/>
             <Route
                     path='directory/:campsiteId'
                     element={<CampsiteDetailPage />}
---
+++ src/features/partners/partnersSlice.js
@@ -0,0 +1,10 @@
+import {PARTNERS} from '../../app/shared/PARTNERS.js';
+
+export const selectAllPartners = () =>{ 
+    return PARTNERS
+}
+export const selectFeaturedPartner = () =>{
+    return(
+        PARTNERS.find((partner)=>partner.featured)
+    );
+}
