2353708e18faccf2fc5fb889b2522c0e405529bc
nassour <nassouryaya1993@gmail.com>
2024-05-07 20:54:40 +0200

    Redux for All

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -14,18 +14,20 @@ const campsitesSlice = createSlice({
 export const campsitesReducer = campsitesSlice.reducer;
 
 
-export const selectAllCampsites=() =>{
-    return CAMPSITES;
+export const selectAllCampsites=(state) =>{
+    return state.campsites.campsitesArray;
 }
 export const selectRandomCampsite = () =>{
     const index=Math.floor(Math.random()*CAMPSITES.length);
     return CAMPSITES[index];
 }
-export const selectFeaturedCampsite = () => {
-    return CAMPSITES.find((campsite)=> campsite.featured);
-}
+export const selectFeaturedCampsite = (state) => {
+    return state.campsites.campsitesArray.find((campsite) => campsite.featured);
+};
 
-export const selectCampsiteById = (id) => {
-    return CAMPSITES.find((campsite) => campsite.id === parseInt(id));
+export const selectCampsiteById = (id) => (state) => {
+    return state.campsites.campsitesArray.find(
+        (campsite) => campsite.id === parseInt(id)
+    );
 };
 
